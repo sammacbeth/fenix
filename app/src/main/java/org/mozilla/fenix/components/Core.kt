4c714996d440d393d0d46f373069431acfdb34ba
Sam Macbeth <sam@cliqz.com>
2019-09-30 14:25:01 +0200

    Base privacy module

--- app/src/main/java/org/mozilla/fenix/components/Core.kt
+++ app/src/main/java/org/mozilla/fenix/components/Core.kt
@@ -8,6 +8,7 @@ import GeckoProvider
 import android.content.Context
 import android.content.res.Configuration
 import com.cliqz.dat.DatFeature
+import com.cliqz.privacy.PrivacyFeature
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -60,6 +61,7 @@ class Core(private val context: Context) {
 
         GeckoEngine(context, defaultSettings, GeckoProvider.getOrCreateRuntime(context)).also {
             DatFeature.install(it)
+            PrivacyFeature.install(it)
         }
     }
 
